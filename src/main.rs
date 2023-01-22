665a7865f743619b93f9a78c0d8ee4e672948500
Breno R <br3eno14@gmail.com>
2023-01-22 02:12:34 -0300

    route for retrieve shortner url

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+mod api;
+mod models;
+mod repository;
+
+use actix_web::{get, web::Data, App, HttpResponse, HttpServer, Responder};
+use api::url_api::{create_url, get_url};
+use repository::mongodb_repo::MongoRepo;
+
+#[get("/_health")]
+async fn health() -> impl Responder {
+    HttpResponse::Ok().body("App Health")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let db = MongoRepo::init().await;
+    let db_data = Data::new(db);
+    HttpServer::new(move || {
+        App::new()
+            .app_data(db_data.clone())
+            .service(create_url)
+            .service(health)
+            .service(get_url)
+    })
+        .bind(("0.0.0.0", 8080))?
+        .run()
+        .await
 }
